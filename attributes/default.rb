491f97ce2af8b8ac36bc2932622c14df0a7cf29a
Sanket Gawade <sgawade@alertlogic.com>
2014-03-25 14:23:40 -0500

    Adding attribute for rundeck project directory and changing versions for the rundeck for Debian and Red Hat

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,11 +20,11 @@
 
 # --- Rundeck packages ---
 
-default['rundeck']['deb_version']       = '2.0.1-1-GA'
+default['rundeck']['deb_version']       = '2.0.3-1-GA'
 default['rundeck']['deb_url']           = "http://download.rundeck.org/deb/rundeck-#{node['rundeck']['deb_version']}.deb"
 default['rundeck']['deb_checksum']      = '314b68c3ad25a29986efb76861cba1993023614d3981d5043f34cdbfe4bf267b'
 
-default['rundeck']['rpm_version']       = '1.6.1-1.4.GA'
+default['rundeck']['rpm_version']       = '2.0.3-1.14.GA'
 default['rundeck']['rpm_url']           = "http://download.rundeck.org/rpm/rundeck-#{node['rundeck']['rpm_version']}.noarch.rpm"
 default['rundeck']['rpm_cfg_url']       = "http://download.rundeck.org/rpm/rundeck-config-#{node['rundeck']['rpm_version']}.noarch.rpm"
 default['rundeck']['rpm_checksum']      = '9fd6c9a9043b5cfbc1013048514d5b294c85eebade322f8ab690cd7c43c6d508'
@@ -39,9 +39,9 @@ default['rundeck']['port']          = 4440
 default['rundeck']['log4j_port']    = 4435
 default['rundeck']['public_rss']    = false
 default['rundeck']['logging_level'] = 'INFO'
-
 default['rundeck']['jaas']          = "internal"
 default['rundeck']['required_role'] = "user"
+default['rundeck']['projects_dir']  = "/var/rundeck/projects"
 
 
 # --- Optional databag for secrets ---
